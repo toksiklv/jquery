29f6010804f83ca94f6f1dd457034e55660719dd
Richard Gibson <richard.gibson@gmail.com>
2011-12-08 14:49:57 -0500

    even DRYer

--- src/css.js
+++ src/css.js
@@ -170,13 +170,11 @@ jQuery.each(["height", "width"], function( i, name ) {
 	jQuery.cssHooks[ name ] = {
 		get: function( elem, computed, extra ) {
 			if ( computed ) {
-				if ( elem.offsetWidth !== 0 ) {
+
+				// Use swap to DRY/save bytes, but don't toggle anything unless offsetWidth is zero
+				return jQuery.swap( elem, elem.offsetWidth === 0 && cssShow, function() {
 					return getWidthOrHeight( elem, name, extra );
-				} else {
-					return jQuery.swap( elem, cssShow, function() {
-						return getWidthOrHeight( elem, name, extra );
-					});
-				}
+				});
 			}
 		},
 
@@ -333,42 +331,36 @@ function getWidthOrHeight( elem, name, extra ) {
 	// Start with offset property
 	var val = name === "width" ? elem.offsetWidth : elem.offsetHeight,
 		i = name === "width" ? 1 : 0,
-		len = 4;
+
+		// Adjustments dependent upon extra. Underscore is the Top/Right/Bottom/Left placeholder.
+		// Remember: false -> 0; true -> 1
+		adjustments = { margin_: extra === "margin" };
 
 	if ( val > 0 ) {
 		if ( extra !== "border" ) {
-			for ( ; i < len; i += 2 ) {
-				if ( !extra ) {
-					val -= parseFloat( jQuery.css( elem, "padding" + cssExpand[ i ] ) ) || 0;
-				}
-				if ( extra === "margin" ) {
-					val += parseFloat( jQuery.css( elem, extra + cssExpand[ i ] ) ) || 0;
-				} else {
-					val -= parseFloat( jQuery.css( elem, "border" + cssExpand[ i ] + "Width" ) ) || 0;
-				}
-			}
+			adjustments.padding_ = -!extra;
+			adjustments.border_Width = -!adjustments.margin_;
+		}
+	} else {
+
+		// Fall back to computed then uncomputed css if necessary
+		val = curCSS( elem, name );
+		if ( val < 0 || val == null ) {
+			val = elem.style[ name ];
 		}
 
-		return val + "px";
+		// Normalize "", auto, and prepare for extra
+		val = parseFloat( val ) || 0;
+
+		// Add padding, border, margin
+		adjustments.padding_ = !!extra;
+		adjustments.border_Width = extra && extra !== "padding";
 	}
 
-	// Fall back to computed then uncomputed css if necessary
-	val = curCSS( elem, name, name );
-	if ( val < 0 || val == null ) {
-		val = elem.style[ name ];
-	}
-	// Normalize "", auto, and prepare for extra
-	val = parseFloat( val ) || 0;
-
-	// Add padding, border, margin
-	if ( extra ) {
-		for ( ; i < len; i += 2 ) {
-			val += parseFloat( jQuery.css( elem, "padding" + cssExpand[ i ] ) ) || 0;
-			if ( extra !== "padding" ) {
-				val += parseFloat( jQuery.css( elem, "border" + cssExpand[ i ] + "Width" ) ) || 0;
-			}
-			if ( extra === "margin" ) {
-				val += parseFloat( jQuery.css( elem, extra + cssExpand[ i ]) ) || 0;
+	for ( ; i < 4; i+=2 ) {
+		for ( name in adjustments ) {
+			if ( adjustments[ name ] ) {
+				val += adjustments[ name ] * parseFloat( jQuery.css( elem, name.replace( "_", cssExpand[ i ] ) ) ) || 0;
 			}
 		}
 	}
